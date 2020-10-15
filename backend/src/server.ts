9ad59f8fbf19c4957dc4fe8792431a3c652d01cf
CinthiaNagahama <56683006+CinthiaNagahama@users.noreply.github.com>
2020-10-15 11:01:13 -0300

    Up
    Ainda tentando finalizar o dia 2 (1:26:59)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,36 +1,10 @@
 import express from 'express';
+import './database/connection';
+import routes from './routes';
 
 const app = express();
 
-app.use(express.json);
-
-// Rota = conjunto
-// Recurso = usuários
-
-/* Método HTTP:
-    GET -> Buscar uma informação (Lista ou item)
-    POST -> Criar uma informação
-    PUT -> Editar uma informação
-    DELETE -> Deletar uma informação
-*/
-
-/* Parâmetros:
-    Query Params -> http://localhost:3333/users?search=diego -> (? busca & concatecação)
-    Route Params -> http://localhost:3333/users/1 -> (Identificar um recurso)
-    Body -> http://localhost:3333/users (Corpo da requisição)
-*/
-/*
-app.post('/users/:id', (request, response) => {
-    console.log(request.query); // Devolve {message: 'Hello World'}
-    console.log(request.params); // Devolve o id
-    console.log(request.body); // Devolve o body
-
-    return response.json({ message: 'Hello World!' });
-});
-*/
-
-app.get('/users', (request, response) => {
-    return response.json({ message: 'Hello Wolrd' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
